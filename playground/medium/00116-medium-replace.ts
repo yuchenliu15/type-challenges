8fa4a7bcd895f85c1e7cf2df6a18715985d5d6b6
yuchenliu15 <yl6448@nyu.edu>
2024-05-29 21:17:46 -0400

    Replace<T>

--- playground/medium/00116-medium-replace.ts
+++ playground/medium/00116-medium-replace.ts
@@ -18,8 +18,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Replace<S extends string, From extends string, To extends string> = any
-
+type Replace<S extends string, From extends string, To extends string> = 
+  From extends '' ? S :
+  S extends `${infer L}${From}${infer R}` ? `${L}${To}${R}` : 
+  S
+type t = Replace<'foobarbar', '', 'foo'>;
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
