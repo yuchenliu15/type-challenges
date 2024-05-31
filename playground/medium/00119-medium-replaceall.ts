ee800e6f9821abdd7454a3ac73a445502f569f07
yuchenliu15 <yl6448@nyu.edu>
2024-05-30 22:58:41 -0400

    ReplaceAll<T, From, To>

--- playground/medium/00119-medium-replaceall.ts
+++ playground/medium/00119-medium-replaceall.ts
@@ -18,7 +18,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<S extends string, From extends string, To extends string> =
+  From extends '' ? S :
+    S extends `${infer L}${From}${infer R}` 
+      ? `${ReplaceAll<L, From, To>}${To}${ReplaceAll<R, From, To>}`
+      : S;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
