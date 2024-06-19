8da94a9772bfa6946405dc3ba7ef4377619d941d
yuchenliu15 <yl6448@nyu.edu>
2024-06-19 11:00:27 -0400

    PickByType<T,U>

--- playground/medium/02595-medium-pickbytype.ts
+++ playground/medium/02595-medium-pickbytype.ts
@@ -23,7 +23,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type PickByType<T, U> = any
+type PickByType<T, U> = {
+  [K in keyof T as T[K] extends U ? K : never] : T[K]
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
