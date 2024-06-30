56098ed0ee1eacebc0658f3efa3615b0f14b6c22
yuchenliu15 <yl6448@nyu.edu>
2024-06-30 12:28:37 -0400

    Unique<T>

--- playground/medium/05360-medium-unique.ts
+++ playground/medium/05360-medium-unique.ts
@@ -19,8 +19,19 @@
 */
 
 /* _____________ Your Code Here _____________ */
+type Includes<T, U> = 
+  U extends [infer F, ...infer Rest]
+    ? Equal<F, T> extends true
+      ? true
+      : Includes<T, Rest>
+    : false
 
-type Unique<T> = any
+type Unique<T, U extends any[] = []> = 
+  T extends [infer F, ...infer Rest] 
+    ? Includes<F, U> extends true
+      ? Unique<Rest, U>
+      : Unique<Rest, [...U, F]>
+    : U
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
